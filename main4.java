ab4ca1a0647aec21259807266efbdcb9a28c6a44
Guy Kongthong <guykongthongcmu@gmail.com>
2025-01-25 16:25:33 +0700

    Update main4.java

--- main4.java
+++ main4.java
@@ -1,20 +1,25 @@
-// 672115045 Virawit Kongthong ADT Tutorial 4
+//672115045 Virawit Kongthong ADT Tutorial 4
+
+import java.util.Scanner;
 
 public class main4 {
     public static void main(String[] args) {
         queueClass testQueue = new queueClass(5);
+        Scanner input = new Scanner(System.in);
 
-        testQueue.enqueue("John");
-        testQueue.printQueue();
-        System.out.println();
+        boolean quit = false;
+        while (quit != true) {
+            System.out.println("Please input names: (Enter Quit to exit the program)");
+            String userInput = input.nextLine();
 
-        testQueue.enqueue("Man");
-        testQueue.printQueue();
-        System.out.println();
-
-        testQueue.enqueue("Pree");
-        testQueue.printQueue();
-        System.out.println();
+            if ((userInput).toLowerCase().equals("quit")) {
+                quit = true;
+            } else {
+                testQueue.enqueue(userInput);
+                testQueue.printQueue();
+                System.out.println();
+            }
+        }
 
         testQueue.dequeue();
         testQueue.printQueue();
